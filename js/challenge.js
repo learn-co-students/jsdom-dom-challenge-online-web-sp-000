b2740741ef7d16756d60ea165158d826a48330ca
Lindsey Wells <github email address>
2020-01-10 12:42:52 +0000

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -8,16 +8,29 @@ function decrement() {
     counter.innerText--
 }
 
-function updateLikes() {
+function createOrUpdateLikes() {
+    if (document.getElementById(`${counter.innerText}`) !== null) {
+        let foundLike = document.getElementById(`${counter.innerText}`)
+        let dataCount = foundLike.getAttribute("data-count")
+        dataCount++
+        foundLike.setAttribute("data-count", dataCount)
+        foundLike.innerHTML = `Number ${counter.innerText}, ${dataCount} likes`
+    } else createLike()
+}
+
+function createLike() {
     let likesList = document.getElementsByClassName("likes")
     let newLike = document.createElement("li")
-    newLike.innerHTML = `Number ${counter.innerText}, +1 like`
+    let dataCount = 1
+    newLike.setAttribute("id", `${counter.innerText}`);
+    newLike.setAttribute("data-count", `${dataCount}`);
+    newLike.innerHTML = `Number ${counter.innerText}, ${dataCount} like`
     likesList[0].appendChild(newLike)
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    setInterval(function() { increment() }, 1000);
-})
+function interval() { setInterval(function() { increment() }, 1000) }
+
+document.addEventListener("DOMContentLoaded", function() { interval() })
 
 const upbutton = document.getElementById("plus");
 upbutton.addEventListener("click", function() {
@@ -31,5 +44,15 @@ downbutton.addEventListener("click", function() {
 
 const likebutton = document.getElementById("heart");
 likebutton.addEventListener("click", function() {
-    updateLikes()
+    createOrUpdateLikes()
+})
+
+const pausebutton = document.getElementById("pause")
+
+function pause() {
+    clearInterval(interval())
+}
+
+pausebutton.addEventListener("click", function() {
+    pause()
 })
