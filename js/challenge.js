4e8b2194333d4c5d3b4315ec6714ed70a11df410
tomciv9000 <eugene.white@gmail.com>
2020-03-09 15:00:46 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,49 +1,48 @@
 let counter_id = document.getElementById('counter')
-let pause_id = document.getElementById("pause")
-let minus_id = document.getElementById("minus")
 let plus_id = document.getElementById("plus")
+let minus_id = document.getElementById("minus")
 let heart_id = document.getElementById("heart")
-let list_id = document.getElementById("list")
+let pause_id = document.getElementById("pause")
 let comment_id = document.getElementsByTagName("form")[0]
 
-let life = true;
+//this is setting the pause 'state' in a variable
+let life = true
 
-let counter = 0;
+let counter = 0
 
 let timer = setInterval(function(){
-    if(life){
-        counter_id.innerHTML = counter
-        counter ++;
-    }
-
-}, 1000);
+  if (life){
+    counter_id.innerHTML = counter
+    counter++;
+  }
+}, 1000)
 
 plus_id.addEventListener("click", function(){
-    counter += 1;
-    counter_id.innerHTML = parseInt(counter_id.innerHTML) + 1
+  counter += 1
+  counter_id.innerHTML = parseInt(counter_id.innerHTML) + 1
 })
-  
+
 minus_id.addEventListener("click", function(){
-    counter -= 1;
-    counter_id.innerHTML = parseInt(counter_id.innerHTML) - 1
+  counter -= 1
+  counter_id.innerHTML = parseInt(counter_id.innerHTML) - 1
 })
 
 heart_id.addEventListener("click", function(){
-    let like = document.querySelector(".likes")
-  //if there isn't a like counter, create one
-    if(document.getElementById(`Li${counter}`) == null){
-      let li = document.createElement("li");
-      li.setAttribute("id", `Li${counter}`)
-      li.innerHTML = `${counter} have this many likes:1`
-      like.appendChild(li)
-    }
-    else {
-      let li = document.getElementById(`Li${counter}`)
-      let splitted = parseInt(li.innerHTML.split(":")[1]) + 1
-      li.innerHTML = `${counter} have this many likes:${splitted}`
-      like.appendChild(li)
-    }
-  
+  let like = document.querySelector(".likes")
+//if there isn't a like counter, create one
+  if(document.getElementById(`Li${counter}`) == null){
+    let li = document.createElement("li");
+    li.setAttribute("id", `Li${counter}`)
+    li.innerHTML = `${counter} have this many likes:1`
+    like.appendChild(li)
+  }
+  else {
+    let li = document.getElementById(`Li${counter}`)
+    let splitted = parseInt(li.innerHTML.split(":")[1]) + 1
+    li.innerHTML = `${counter} have this many likes:${splitted}`
+    like.appendChild(li)
+  }
+
 })
 
 pause_id.addEventListener("click",function(){
@@ -55,17 +54,16 @@ pause_id.addEventListener("click",function(){
     pause_id.innerHTML = "pause"
     life = true
   }
-
 })
 
 comment_id.addEventListener("submit",function(a){
-    a.preventDefault()
-    let b=this.children[0]
-    let c=b.value;
-    //resets the input text field for comments
-    b.value="";
-    let d=document.querySelector(".comments")
-    let e=document.createElement("p")
-    e.innerText=c
-    d.appendChild(e)
-  })
+  a.preventDefault()
+  let commentField = this.children[0]
+  let comment = commentField.value;
+  //resets the input text field for comments
+  commentField.value="";
+  let commentsDiv = document.querySelector(".comments")
+  let pComment = document.createElement("p")
+  pComment.innerText = comment
+  commentsDiv.appendChild(pComment)
+})
