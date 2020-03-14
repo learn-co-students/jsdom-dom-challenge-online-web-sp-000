77fc14f470c2f1f2bfce7a2ffe7c95128b1eebf1
BlythePollard <github email address>
2020-03-13 21:02:08 -0600

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,51 +1,57 @@
     
-document.addEventListener("DOMContentLoaded", startCounter)
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("pause").addEventListener("click", decideCounter);
+    startCounter()
+}  ) 
 
     var el = document.getElementById("counter");
     var counter = parseInt(el.innerHTML)
     var pauseResume = document.getElementById("pause")
+    var submitButton = document.getElementById("submit")
+    var plusButton = document.getElementById("plus")
+    var minusButton = document.getElementById("minus")
+    var heartButton = document.getElementById("heart")
     var intervalId
-
+    var isPaused = false 
+    
     function startCounter() {
         intervalId = setInterval(function() {
-            counter++
-            el.innerHTML = counter
+            if(!isPaused) {
+                counter++
+                el.innerHTML = counter
+            }
         }, 1000);
     }
 
-    function pauseCounter() {
-        clearInterval(intervalId) 
-        document.getElementById("pause").innerHTML = "resume"
-        document.getElementById("submit").disabled = true 
-        document.getElementById("plus").disabled = true  
-        document.getElementById("minus").disabled = true 
-        document.getElementById("heart").disabled = true 
+    function toggleDisable(paused) { //change naming here?
+        submitButton.disabled = paused 
+        plusButton.disabled = paused  
+        minusButton.disabled = paused 
+        heartButton.disabled = paused 
     }
     
     function decideCounter() {
-        if (pauseResume.innertext == "resume") {
-          pauseResume.addEventListener("click", startCounter)
-      } else if (pauseResume.innertext == "pause") {
-          pauseResume.addEventListener("click", pauseCounter)
+        if (isPaused) {
+          isPaused = false
+           pauseResume.innerHTML = "pause"
+      } else {
+          isPaused = true
+          pauseResume.innerHTML = "resume"
      }
-    } //this does not work (wrapped in a function with another eventlistener or not), but feels like it should!
-    pauseResume.addEventListener("click", decideCounter); //this + pauseCounter works to pause
-
-
-
-
+     toggleDisable(isPaused)
+    } 
 
     function incrementCounter() {
-        document.getElementById("plus").onclick = counter.value = ++counter;
+        plusButton.onclick = counter.value = ++counter;
         el.innerHTML = counter
     }
-    document.getElementById("plus").addEventListener("click", incrementCounter)
+    plusButton.addEventListener("click", incrementCounter)
 
     function decrementCounter() {
-        document.getElementById("minus").onclick = counter.value = --counter;
+        minusButton.onclick = counter.value = --counter;
         el.innerHTML = counter
     }
-    document.getElementById("minus").addEventListener("click", decrementCounter)
+    minusButton.addEventListener("click", decrementCounter)
 
     function likeNumber() {
         const likesArea = document.querySelector('ul')
@@ -53,7 +59,7 @@ document.addEventListener("DOMContentLoaded", startCounter)
         likes.innerHTML = `${counter} has been liked 1 time` 
         likesArea.appendChild(likes)
     } //need to modify string for how many times liked: do I need to do an incrementer here?
-    document.getElementById("heart").addEventListener("click", likeNumber)
+    heartButton.addEventListener("click", likeNumber)
 
     function leaveComment() {
         event.preventDefault();
@@ -64,6 +70,6 @@ document.addEventListener("DOMContentLoaded", startCounter)
         ul.appendChild(comments)
         comments.innerHTML = document.getElementById('comment-input').value
     }
-    document.getElementById("submit").addEventListener("click", leaveComment)
+    submitButton.addEventListener("click", leaveComment)
 
 
