4545092014b2fd6056850578d59f3f59bb720195
Ronald Stephens <rstephens@bluemorpho.cc>
2020-03-19 13:28:47 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -30,3 +30,58 @@ document.addEventListener("DOMContentLoaded", () => {
     button.addEventListener("click", pause);
 })
 // end of pause button code
+
+// plus & minus buttons
+
+let elPlus = document.getElementById('plus');
+let elMinus = document.getElementById('minus');
+
+function plus() {
+    seconds++;
+}
+
+function minus() {
+    seconds--;
+}
+
+elPlus.addEventListener('click', plus, false);
+elMinus.addEventListener('click', minus, false);
+
+// end of plus & minus buttons
+
+// like button
+let elHeart = document.getElementById('heart');
+
+
+function like() {
+    let li = document.createElement('li');
+    let like = document.createTextNode(`${seconds} has been liked ${clickCounter()} time`);
+    li.appendChild(like);
+    let position = document.getElementsByTagName('ul')[0];
+    position.appendChild(li);
+}
+
+function clickCounter() {
+    let value = parseInt(document.getElementById('heart').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    return value;
+}
+
+elHeart.addEventListener('click', like, false);
+
+// end of likes
+
+//leave a comment
+
+let comment = document.getElementById('comment-input');
+let submit = document.getElementById('submit');
+
+function post() {
+    let p = document.createElement('p');
+    p.appendChild(comment);
+    let placement = document.getElementById('list');
+    placement.appendChild(p);
+}
+
+submit.addEventListener('click', post, false);
