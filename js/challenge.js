cfbd77a9e3ff8ebb677f6f43c20e8ae764f40b64
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-27 20:39:51 -0400

    plus and minus as well as basic pause

--- js/challenge.js
+++ js/challenge.js
@@ -1,16 +1,53 @@
+// Variable declaration
 let counter = document.getElementById('counter');
-
-// Comments
+let pause = document.getElementById('pause');
+let minus = document.getElementById('minus');
+let plus = document.getElementById('plus');
+let heart = document.getElementById('heart');
 let comments = document.getElementById('list');
 let commentForm = document.getElementById('comment-form');
 const user_input = document.getElementById('comment-input');
-let ul = document.createElement('ul');
-comments.appendChild(ul);
+let submit = document.getElementById('submit');
+let paused = false;
 
+// Comment submission
 commentForm.addEventListener('submit', function(event){
-  let li = document.createElement('li');
-  li.innerHTML = user_input.value;
-  ul.appendChild(li);
+  let newComment = document.createElement('p');
+  newComment.innerHTML = user_input.value;
+  comments.appendChild(newComment);
   event.preventDefault();
   user_input.value = "";
+});
+
+// Updating the counter
+function updateCounterTime(value) {
+  let counterInt = parseInt(counter.innerText);
+  counterInt += value;
+  counter.innerText = counterInt.toString();
+};
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//   counter.setInterval(updateCounterTime(), 1000, 1);
+// })
+
+minus.addEventListener('click', function(event){
+  updateCounterTime(-1);
+});
+
+plus.addEventListener('click', function(event){
+  updateCounterTime(1);
+});
+
+// Pressing pause - disable all buttons
+pause.addEventListener('click', function(event){
+  submit.disabled = !submit.disabled;
+  plus.disabled = !plus.disabled;
+  minus.disabled = !minus.disabled;
+  heart.disabled = !heart.disabled;
+  if (paused) {
+    pause.innerText = "pause";
+  } else {
+    pause.innerText = "resume";
+  }
+  paused = !paused;
 });
