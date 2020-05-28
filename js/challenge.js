389b177bc7bc04db11022f7a2c568ff40ba118a8
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-27 21:12:08 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -8,16 +8,9 @@ let comments = document.getElementById('list');
 let commentForm = document.getElementById('comment-form');
 const user_input = document.getElementById('comment-input');
 let submit = document.getElementById('submit');
+let likesList = document.getElementsByTagName('ul')[0];
 let paused = false;
-
-// Comment submission
-commentForm.addEventListener('submit', function(event){
-  let newComment = document.createElement('p');
-  newComment.innerHTML = user_input.value;
-  comments.appendChild(newComment);
-  event.preventDefault();
-  user_input.value = "";
-});
+let likes = [];
 
 // Updating the counter
 function updateCounterTime(value) {
@@ -44,12 +37,41 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     paused = !paused;
   });
-});
 
-minus.addEventListener('click', function(event){
-  updateCounterTime(-1);
-});
+  // Pressing minus - subtract one from the timer
+  minus.addEventListener('click', function(event){
+    updateCounterTime(-1);
+  });
 
-plus.addEventListener('click', function(event){
-  updateCounterTime(1);
+  // Pressing plus - add one to the timer
+  plus.addEventListener('click', function(event){
+    updateCounterTime(1);
+  });
+
+  // Pressing the like button - add one to the current amount in the list
+  heart.addEventListener('click', function(event) {
+    let counterInt = parseInt(counter.innerText);
+
+    if (typeof likes[counterInt] === 'undefined') {
+      likes[counterInt] = 1;
+      let li = document.createElement('li');
+      li.innerHTML = `${counterInt} has been liked 1 time`;
+      li.id = `likes-${counterInt}`;
+      likesList.appendChild(li);
+    } else {
+      likes[counterInt] += 1;
+      let li = document.getElementById(`likes-${counterInt}`);
+      li.innerHTML = `${counterInt} has been liked ${likes[counterInt]} times`;
+    }
+
+  });
+
+  // Comment submission
+  commentForm.addEventListener('submit', function(event){
+    let newComment = document.createElement('p');
+    newComment.innerHTML = user_input.value;
+    comments.appendChild(newComment);
+    event.preventDefault();
+    user_input.value = "";
+  });
 });
