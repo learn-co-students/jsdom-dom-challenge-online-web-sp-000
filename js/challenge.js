7c5f86b6a4a06f213bff6cef37d51d64f386ba2e
Christopher Dent <christopherdent01@gmail.com>
2020-06-23 21:08:08 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,94 +1,74 @@
-const form = document.getElementById('comment-form');
-const pauseButton = document.getElementById('pause')
-const plusButton = document.getElementById('plus')
-const minusButton = document.getElementById('minus')
-const likeButton = document.getElementById('heart')
-let likedNumbers = new Map()
-let paused = false
-let intervalID;
 
-plusButton.addEventListener('click', plusTime)
-minusButton.addEventListener('click', minusTime)
-likeButton.addEventListener('click', clickedLike)
-pauseButton.addEventListener('click', pause)
+let counter = document.getElementById("counter")
+let minus = document.getElementById("minus")
+let plus = document.getElementById("plus")
+let pause = document.getElementById("pause")
+let heart = document.getElementById("heart")
+let b = document.getElementById("submit")
+let count = parseInt(counter.innerText)
+  function countup() {
+    counter.innerText = count += 1
+    pause.innerText = "pause"
+  }
 
 
-setTimeout(start, 5000);
-  var i = 1;
-  var num = document.getElementById("counter");
-function start() {
-  setInterval(increase, 1000);
+let intId = setInterval(countup, 1000)
+
+function resume(){
+  setInterval(countup, 1000)
+  pause.innerText = "pause"
 }
 
-function increase() {
-    if (i < 10000000000) {
-      i++;
-      num.innerText = i;
-    }
+function subtract() {
+  counter.innerText = count -= 1;
+}
+minus.addEventListener("click", subtract);
+
+function add(){
+  counter.innerText = count += 1;
+}
+plus.addEventListener("click", add);
+
+function stop(){
+  clearInterval(intId);
+  pause.innerText = "unpause"
 }
 
 
-document.addEventListener("DOMContentLoaded", () => {
-
-
-let taskDescription = document.getElementById('comment-input');
-
-function addComment (event) {
-  const comment = document.createElement('p');
-  comment.textContent = taskDescription.value;
-  list.appendChild(comment);
-  event.preventDefault();
-}
-form.addEventListener("submit", addComment, false);
-});
-
-function pause() {
-    if (!paused) {
-        paused = true
-        clearInterval(intervalID)
-        pauseButton.innerHTML = 'resume'
-        likeButton.disabled = true
-        plusButton.disabled = true
-        minusButton.disabled = true
-        return !increase
-    } else {
-        paused = false
-        increase()
-        pauseButton.innerHTML = 'pause'
-        likeButton.disabled = false
-        plusButton.disabled = false
-        minusButton.disabled = false
-        return increase
-    }
-}
-
-function plusTime(){
-  let number = parseInt(counter.innerHTML, 10)
-  counter.innerHTML = `${number + 1}`
-}
-
-function minusTime(){
-  let number = parseInt(counter.innerHTML, 10)
-  if (number > 0) {
-      counter.innerHTML = `${number - 1}`
+function stopstart() {
+if (pause.innerText === "pause"){
+  pause.addEventListener("click", stop)
+  } else if (pause.innerText === "unpause"){
+    pause.addEventListener("click", resume)
   }
 }
 
-function clickedLike() {
-  let number = parseInt(counter.innerHTML, 10)
-  let ul = document.querySelector('ul.likes')
+pause.addEventListener("click", stopstart)
+
+function like(){
+// console.log(`I liked the number ${count}`)
+    let div = document.createElement('div')
+     div.id = 'likes'
+     document.body.appendChild(div)
+     let ul = document.createElement('ul')
+
   let li = document.createElement('li')
-  let count = 1
-
-  if (likedNumbers.has(number)) {
-      count = likedNumbers.get(number) + 1
-      li = document.querySelector('[data-num="'+ CSS.escape(number)+ '"]')
-      li.innerHTML = `${number} number has been liked ${count} times`
-      likedNumbers.set(number, count)
-  } else {
-      li.setAttribute('data-num', number)
-      li.innerHTML = `${number} number has been liked ${count} times`
-      ul.appendChild(li)
-      likedNumbers.set(number, count)
-  }
+  li.innerText = `I liked the number ${count}`
+  ul.appendChild(li)
+  div.appendChild(ul)
 }
+
+heart.addEventListener("click", like)
+
+b.addEventListener("click", function(event){
+   event.preventDefault();
+   let comment = document.getElementById("comment-input")
+   let div = document.createElement('div')
+     div.id = 'comments'
+     document.body.appendChild(div)
+     let ul = document.createElement('ul')
+     let li = document.createElement('li')
+     li.innerText = comment.value
+  ul.appendChild(li)
+  div.appendChild(ul)
+})
