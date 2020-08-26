d14e320d50b8c9d5f84e4bca305031b08c03a778
connorworthen <connorw220@protonmail.com>
2020-08-26 14:49:41 -0700

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 function counterIncreaseByOne() {
   let counterIncrement = document.getElementById("counter");
   let counterCount = parseInt(counterIncrement.innerText); //parseint takes the 0 in the counterIncrement and makes it into an integer
-  counterIncrement.innerText = counterCount += 1; // we set the counterCount to add 1
+  counterIncrement.innerText = counterCount += 1;
 }
 
 pause.addEventListener("click", (e) => {
