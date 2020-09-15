aff321862153d86d1947e72f9321a08d9f372de5
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-15 10:22:44 +0000

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -5,7 +5,6 @@ function _toConsumableArray(a) {
   if (Array.isArray(a)) {
     for (var b = 0, c = Array(a.length); b < a.length; b++)
     c[b]=a[b];
-    //
     return c
   }
     return Array.from(a)
@@ -17,10 +16,9 @@ function _toConsumableArray(a) {
       return setInterval(function() {
         var a = document.getElementById("counter"),
         b = parseInt(a.innerText);
-        a.innerText = b + 1 },
-        1e3)
-      },
+        a.innerText = b+1}, 1e3)},
 
+//grabbing html
         interval = timer(),
         minus = document.getElementById("minus"),
         plus = document.getElementById("plus"),
@@ -28,43 +26,46 @@ function _toConsumableArray(a) {
         pause = document.getElementById("pause"),
         commentForm = document.getElementsByTagName("form")[0];
 
+//subtracting a second from the timer
         minus.addEventListener("click", function() {
           var a = document.getElementById("counter"),
           b = parseInt(a.innerText);
           a.innerText = b-1
         }),
 
+//adding a second to the timer
         plus.addEventListener("click",function() {
-            var a = document.getElementById("counter"),
-            b = parseInt(a.innerText);
-            a.innerText = b+1
+            var a=document.getElementById("counter"),
+            b=parseInt(a.innerText);
+            a.innerText=b+1
           }),
 
+//creating a "like" for the second
         heart.addEventListener("click",function() {
-              var a=document.getElementById("counter"),
+              var a = document.getElementById("counter"),
               b = parseInt(a.innerText),
               c = document.querySelector(".likes"),
               d = void 0;
-              console.log(_toConsumableArray(c.children))
               if ([].concat(_toConsumableArray(c.children)).map(function(a) {
-                return parseInt(a.dataset.num)}).includes(b)){d =d ocument.querySelector('[data-num="'+b+'"]');
+                return parseInt(a.dataset.num)}).includes(b)){d=document.querySelector('[data-num="'+b+'"]');
                 var e = parseInt(d.children[0].innerText);
-                d.innerHTML = b + " has been liked <span>" +(e+1)+ "</span> times"}
+                d.innerHTML = b + " has been liked <span>" + (e + 1) + "</span> times"}
               else
-                (d=document.createElement("li")).setAttribute("data-num", b),
-                d.innerHTML = b+ " has been liked <span>1</span> time", c.appendChild(d)
-              }),
+                (d = document.createElement("li")).setAttribute("data-num", b),
+                d.innerHTML = b + " has been liked <span>1</span> time",
+                c.appendChild(d)}),
 
+//pause seconds
           pause.addEventListener("click",
                   function() {
                     playing ? (playing =! 1,
                       clearInterval(interval),
                       this.innerText="resume") : (playing =! 0, interval = timer(), this.innerText="pause"),
-                [].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a){"pause" !== a.id && (a.disabled =! playing)
-              })
+                [].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a){"pause"!==a.id&&(a.disabled=!playing)})
               }),
 
-            commentForm.addEventListener("submit", function(a) {a.preventDefault();
+//comment
+          commentForm.addEventListener("submit", function(a) {a.preventDefault();
                   var b = this.children[0],
                   c = b.value;
                   b.value = "";
