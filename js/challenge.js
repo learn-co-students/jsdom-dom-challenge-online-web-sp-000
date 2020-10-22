4312e70c7fdb8aadd02ce5ebb5fe8364c7420309
chienleow <chienleow1@gmail.com>
2020-10-22 00:11:14 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -6,23 +6,32 @@
 // disable all buttons except the pause button
 // the pause button should then show the text "resume."
 // When 'resume' is clicked, it should restart the counter and re-enable the buttons.
-// As a user, I can leave comments on my gameplay, such as: "Wow, what a fun game this is."
+// (x) As a user, I can leave comments on my gameplay, such as: "Wow, what a fun game this is."
 
 const plus = document.querySelector("#plus");
 const minus = document.querySelector("#minus");
 const heart = document.querySelector("#heart");
+const likes = document.querySelector(".likes");
 const pause = document.querySelector("#pause");
 const counter = document.querySelector("#counter");
 const secondCounter = window.setInterval(keepCounting, 1000);
 const comments = document.querySelector("#list");
-const commentForm = document.querySelector("#comment-form")
-const commentInput = document.querySelector("#comment-input")
+const commentForm = document.querySelector("#comment-form");
+const commentInput = document.querySelector("#comment-input");
 
 plus.addEventListener("click", countUp);
 minus.addEventListener("click", countDown);
-commentForm.addEventListener('submit', function(e) {
-    e.preventDefault()
-    newParagraph.textContent = newComment
+heart.addEventListener("click", addHeart);
+
+
+commentForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const newComment = commentInput.value;
+    newParagraph = document.createElement('p');
+    newParagraph.innerText = newComment;
+    comments.appendChild(newParagraph);
+
+    commentForm.reset();
 })
 
 function keepCounting() {
@@ -37,6 +46,13 @@ function countDown() {
     if (counter.textContent > 0) {
         counter.textContent--;
     }
+}
+
+function addHeart() {
+    const currentCount = counter.textContent;
+    newLi = document.createElement('li');
+    newLi.innerText = currentCount;
+    likes.appendChild(`${newLi} has been liked ${} time );
 }
 
 
