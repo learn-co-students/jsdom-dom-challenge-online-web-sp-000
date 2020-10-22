fe7c87b61c32afb04154b92f7333e2b249a7809c
chienleow <chienleow1@gmail.com>
2020-10-21 23:41:31 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,5 +1,5 @@
-// As a user, I should see the timer increment every second once the page has loaded.
-// As a user, I can manually increment and decrement the counter using the plus and minus buttons.
+// (x) As a user, I should see the timer increment every second once the page has loaded.
+// (x) As a user, I can manually increment and decrement the counter using the plus and minus buttons.
 // As a user, I can 'like' an individual number of the counter. I should see count of the number of 'likes' associated with that number.
 // As a user, I can pause the counter, which should
 // pause the counter
@@ -8,17 +8,44 @@
 // When 'resume' is clicked, it should restart the counter and re-enable the buttons.
 // As a user, I can leave comments on my gameplay, such as: "Wow, what a fun game this is."
 
-const plus = document.querySelector("#plus")
-const minus = document.querySelector("#minus")
-const heart = document.querySelector("#heart")
-const pause = document.querySelector("#pause")
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const heart = document.querySelector("#heart");
+const pause = document.querySelector("#pause");
+const counter = document.querySelector("#counter");
+const secondCounter = window.setInterval(keepCounting, 1000);
+const comments = document.querySelector("#list");
+const commentForm = document.querySelector("#comment-form")
+const commentInput = document.querySelector("#comment-input")
 
-document.addEventListener("DOMContentLoaded", () => {
-    setInterval(function(){
-        // add to the timer
-        1000
-    });
+plus.addEventListener("click", countUp);
+minus.addEventListener("click", countDown);
+commentForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    newParagraph.textContent = newComment
+})
+
+function keepCounting() {
+    counter.textContent++;
+}
+
+function countUp() {
+    counter.textContent++;
+}
+
+function countDown() {
+    if (counter.textContent > 0) {
+        counter.textContent--;
+    }
+}
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     setInterval(function(){
+//         // add to the timer
+//         1000
+//     });
 
 
 
-});
+// });
