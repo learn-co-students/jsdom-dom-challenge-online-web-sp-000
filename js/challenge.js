55f58b746eb859191cba43f6cd195f85ec444efa
chienleow <chienleow1@gmail.com>
2020-10-22 23:47:16 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -14,7 +14,7 @@ const heart = document.querySelector("#heart");
 const likes = document.querySelector(".likes");
 const pause = document.querySelector("#pause");
 const counter = document.querySelector("#counter");
-const secondCounter = window.setInterval(keepCounting, 1000);
+const everySecondCounter = window.setInterval(keepCounting, 1000);
 const comments = document.querySelector("#list");
 const commentForm = document.querySelector("#comment-form");
 const commentInput = document.querySelector("#comment-input");
@@ -22,7 +22,6 @@ const commentInput = document.querySelector("#comment-input");
 plus.addEventListener("click", countUp);
 minus.addEventListener("click", countDown);
 heart.addEventListener("click", addLike);
-
 
 commentForm.addEventListener("submit", function(e) {
     e.preventDefault();
@@ -35,25 +34,25 @@ commentForm.addEventListener("submit", function(e) {
 })
 
 function keepCounting() {
-    counter.textContent++;
+    const currentCount = parseInt(counter.textContent++, 10);
 }
 
 function countUp() {
-    counter.textContent++;
+    const currentCount = parseInt(counter.textContent++, 10);
 }
 
 function countDown() {
     if (counter.textContent > 0) {
-        counter.textContent--;
+        const currentCount = parseInt(counter.textContent--, 10);
     }
 }
 
 function addLike() {
     const currentCount = parseInt(counter.textContent, 10);
-    const previousLike = document.querySelectorAll('.likes > li'); // longer code
-    // const previousLike = Array.from( likes.children );
-    console.log(previousLike);
-    previousLike.find(previousLike => {
+    // const previousLikes = document.querySelectorAll('.likes > li'); // longer code
+    const previousLikes = Array.from( likes.children );
+    console.log(previousLikes);
+    previousLikes.find(previousLike => {
         const previousLikeCount = parseInt(previousLike.textContent.slice(0, 1), 10);
         return previousLikeCount === currentCount;
     })
@@ -63,8 +62,8 @@ function addLike() {
         // console.log(previousLike.textContent);
         previousLike.textContent = `${currentCount} has been liked ${numberOfHearts + 1} times`;
     } else {
-        const newLi = document.createElement('li');
+        const newLi = document.createElement("li");
         newLi.textContent = `${currentCount} has been liked 1 time`;
-        likes.appendChild(newLi);
+        likes.append(newLi);
     }
 }
