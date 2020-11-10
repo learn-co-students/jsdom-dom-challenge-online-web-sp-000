f2f67be662f8fb3a63c4aab6ff76769584f271c4
github username <jaycru06@yahoo.com>
2020-11-10 00:40:30 -0500

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -10,17 +10,75 @@
 //   When 'resume' is clicked, it should restart the counter and re-enable the buttons.
 // 5. As a user, I can leave comments on my gameplay, such as: "Wow, what a fun game this is."
 document.addEventListener("DOMContentLoaded", function() {
-   setTimer();
+   runTimer();
+   adjustTimer();
+   handleLikes();
 });
 
-function setTimer() {
+function adjustTimer() {
    let counter = document.querySelector("#counter");
+   let counterNum = parseInt(counter.innerText);
+   let minusButton = document.querySelector("#minus");
+   let plusButton = document.querySelector("#plus");
+
+   plusButton.addEventListener("click", function (event) {
+      // debugger;
+      // Increment timer when 0 or greater
+      if (counterNum >= 0) {
+         console.log(event.target);
+         counter.innerText = counterNum++;
+      }
+   })
+
+   // Toggle minus button on timer
+   minusButton.addEventListener("click", function (event) {
+      // Decrement counter when greater than 0
+      if (counterNum > 0) {
+         console.log(event.target);
+         counter.innerText = counterNum--;
+      }
+   })
+}
+
+function runTimer() {
+   let isPaused = false;
    let intervalId = window.setInterval(timerCallback, 1000);
+   let pause = document.querySelector("#pause");
+   // Stop timer when pause button clicked
+   pause.addEventListener("click", function (event) {
+      event.preventDefault();
+      clearInterval(intervalId);
+      isPaused = true;
+   })
+}
+
+function stopTimer() {
+   let timeoutId = setTimeout(stop, 3000);
+}
+
+function stop() {
+
 }
 
 function timerCallback() {
-   let count = 1;
-   while (count < 100) {
-      console.log(count);
-   }
+   let counter = document.querySelector("#counter");
+   let counterNum = parseInt(counter.innerText);
+   counter.innerText = counterNum+1;
+   console.log(counterNum);
+}
+
+function handleLikes() {
+   let likes = document.querySelector(".likes");
+   let heartButton = document.querySelector("#heart");
+   let likeCount = 0;
+   // Handle when user press like button
+   heartButton.addEventListener("click", function (event) {
+      // Determine number user liked and add it to likes
+      let counterNum = parseInt(counter.innerText);
+      let like = document.createElement("li");
+      like.className = "like";
+      likeCount += 1;
+      like.innerHTML = `${counterNum} liked ${likeCount} times.`;
+      likes.appendChild(like);
+   })
 }
