14970c30f0c3ee8a246ec17dd0b6562d1035ee7d
Yumacorn <ReachRyanMui@gmail.com>
2020-11-22 14:11:47 -0500

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -3,6 +3,7 @@ let addCount = document.getElementById("plus");
 let subtractCount = document.getElementById("minus");
 let pause = document.getElementById("pause");
 let clock = setInterval(countUp, 1000);
+let likeButton = document.getElementById("heart");
 
 document.addEventListener('DOMContentLoaded', (e) => {
     console.log('DOM fully loaded and parsed');
@@ -19,11 +20,17 @@ function countDown() {
 function pauseCount() {
     clearInterval(clock);
     pause.innerText = "resume";
+    plus.disabled = true;
+    minus.disabled = true;
+    heart.disabled = true;
 }
 
 function resumeCount() {
     clock = setInterval(countUp, 1000);
     pause.innerText = "pause";
+    plus.disabled = false;   
+    minus.disabled = false;
+    heart.disabled = false;
 }
 
 addCount.addEventListener('click', (e) => {
