5103eca75208d5e3f3e138aea44e817acabe858d
milos381 <milos_popovic@outlook.com>
2020-12-29 01:25:22 -0500

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,21 +1,26 @@
 let seconds = 0;
 let clicks = 0;
+let temp = null;
 let el = document.getElementById('counter');
 let pause = document.getElementById('pause');
 let minus = document.getElementById('minus');
 let plus = document.getElementById('plus');
 let heart = document.getElementById('heart');
 let ul = document.querySelector('ul');
-//pause.addEventListener('click', function () {
-//	pause = false;
-//});
+let likes = document.getElementById('likes');
+pause == true;
+pause.addEventListener('click', function () {
+	pause = false;
+});
+
 function incrementSeconds() {
-
-    //if (!pause) {
-        seconds += 1;
-        el.innerText = seconds;
-    //}
-
+    seconds += 1;
+    if (!pause) {
+        clearInterval(counter);
+        return; 
+    }
+    el.innerText = seconds;
+// the pause as itended does not do what it is supposed to do
 }
 minus.addEventListener('click', function () {
     seconds -= 1;
@@ -25,13 +30,29 @@ plus.addEventListener('click', function () {
     seconds += 1;
     el.innerText = seconds;
 });
-heart.addEventListener('click', function () {
-    clicks += 1;
+/*heart.addEventListener('click', function () {
     let hello = document.createElement('li');
-    hello.innerText = el.innerText + " " + "was liked" + " " + clicks + " " + "times!";
+    likes.dataset.value++
+    hello.innerText = el.innerText + " " + "was liked" + " " + likes.dataset.value + " " + "times!";
     ul.appendChild(hello);
-    clicks = 0;
+// this way the incremented values stay
+});*/
+heart.addEventListener('click', function () {
+    let hello = document.createElement('li');
+    if (temp == el.innerText){
+        clicks++;
+        console.log(temp);
+        hello.innerText = el.innerText + " " + "was liked" + " " + clicks + " " + "times!";
+    } else {
+        clicks = 1;
+        hello.innerText = el.innerText + " " + "was liked" + " " + clicks + " " + "times!";
+    }
+    temp = el.innerText;
+    
+    
+   
+    ul.appendChild(hello);
 
-// the problem I have is that it counts the likes the wrong way
+// this way it doesn't increment the amount of likes but creates new 'li' for each
 });
 let cancel = setInterval(incrementSeconds, 1000);
