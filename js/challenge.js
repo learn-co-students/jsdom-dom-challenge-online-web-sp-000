b174c9f25542910f2bdc2452f4988ca0f09e9419
kkirby16 <kkirby16@my.whitworth.edu>
2021-04-19 12:06:04 -0700

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,20 +1,30 @@
-let interval = setInterval(start_interval, 1000) 
+document.addEventListener("DOMContentLoaded", ()=> { 
+    let minus_button = document.getElementById("minus");
+    let plus_button = document.getElementById("plus");
+    let heart_button = document.getElementById("heart") 
+    let submit_button = document.getElementById("submit");
+    let pause_button = document.getElementById("pause");
+    let counterElement = document.getElementById("counter");
 
+
+let interval = setInterval(start_interval, 1000)
+let pause;
 
   // get element, set element, update dom
 //have start interval. could be hidden value somewhere counting off. or could have interval = x or a variable and then += to have it tick along. 
 //separate counter that would count off mycounter.
 function start_interval() { 
-    let counterElement = document.getElementById("counter");
     let current_counter = document.getElementById("counter").innerHTML;
         let integer_counter = parseInt(current_counter, 10) + 1;
         counterElement.innerText = integer_counter;
-        let pause_button = document.getElementById("pause");
-        pause_button.addEventListener("click", (event)=> {pauseCounter(event, interval)})
+        pause = false;
 };
 
 
+        pause_button.addEventListener("click", (event)=> {pauseCounter(event, interval)})
 
+
+//set interval saving interval id to variable, clear interval removing the interval id. 
 
 document.getElementById("plus").addEventListener("click", manuallyIncrement);
 
@@ -36,21 +46,49 @@ document.getElementById("counter").innerHTML = integer_counter;
 }
 
 function pauseCounter(event, interval) { 
-clearInterval(interval);
+if (pause == false) { 
+    clearInterval(interval);
+
 document.getElementById("minus").disabled = true;
 document.getElementById("plus").disabled = true;
 document.getElementById("heart").disabled = true;
 document.getElementById("minus").disabled = true;
 document.getElementById("submit").disabled = true;
 document.getElementById("pause").innerText = "resume";
-let resume_button = document.getElementById("pause");
-resume_button.addEventListener("click", function(event) { 
-    interval;
-});
+pause = true; 
+// let resume_button = document.getElementById("pause");
+// resume_button.addEventListener("click", resume);
+}
+else {
+resume();
+}
 }
 //variable = false
 //if {variable = false}
 //variable = true 
+
+function resume() { 
+clearInterval(interval);
+    //start_interval();
+   interval = setInterval(start_interval, 1000);
+   document.getElementById("minus").disabled = false;
+document.getElementById("plus").disabled = false;
+document.getElementById("heart").disabled = false;
+document.getElementById("minus").disabled = false;
+document.getElementById("submit").disabled = false;
+pause_button.innerText = "pause"
+
+    // let pause_button = document.getElementById("pause");
+    // pause_button.addEventListener("click", function(event) {
+    //     pause_button.innerText = "resume";
+    //     pauseCounter();
+    // });
+    //changing event listener whether you're on resume or a pause button. 
+}
+
+
+
+
 
 document.getElementById("submit").addEventListener("submit", formSubmit );
 
@@ -58,10 +96,12 @@ function formSubmit(event) {
     event.preventDefault();
     let ul = document.createElement("ul");
     let li = document.createElement("li");
-    let div = document.getElementsByTagName("div");
-    li.innerHTML += document.getElementById("comment-input").value;
+    let list_div = document.getElementById("list");
+    li.innerText += document.getElementById("comment-input").value;
     ul.appendChild(li);
-    div.appendChild(ul);
+    list_div.appendChild(ul);
+    console.log(document.getElementById("comment-input").value);
+
 }
 
-
+})
